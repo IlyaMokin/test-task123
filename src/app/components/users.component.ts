065204ccf81b4b0474fd7d3e5b3aab4b185f7534
nastyamyalik <nastia.15@inbox.ru>
2018-08-22 16:20:49 +0300

    fix interface

--- src/app/components/users.component.ts
+++ src/app/components/users.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../services/api.service';
-import { Users } from './users.interface';
+import { User } from './user.interface';
 @Component({
     selector: 'users',
     templateUrl: './users.component.html',
     styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
-    users: Array<Users>;
+    users: Array<User>;
     constructor(private apiService: ApiService) {
     }
 
